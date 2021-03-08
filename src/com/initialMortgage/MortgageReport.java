70987384f44540d46877bf274651d7cc46a7918a
AlidaSchefers <alidadschefers@gmail.com>
2021-03-08 08:15:25 -0500

    initial commit

--- src/com/initialMortgage/MortgageReport.java
+++ src/com/initialMortgage/MortgageReport.java
@@ -1,2 +1,25 @@
-package com.initialMortgage;public class MortgageReport {
+package com.initialMortgage;
+
+import java.text.NumberFormat;
+
+public class MortgageReport {
+
+    private MortgageCalculator calculator;
+
+    public MortgageReport(MortgageCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public void printPaymentSchedule() {
+        System.out.println("\n" + "PAYMENT SCHEDULE" + "\n" + "----------------");
+        for (short month = 0; month <= calculator.getPeriodYears() * MortgageCalculator.MONTHS_IN_YEAR; month++)
+            System.out.println(NumberFormat.getCurrencyInstance().format(calculator.calcRemainingBalance(month)));
+    }
+
+    public void printMortgage() {
+        double mortgage = calculator.calculateMortgage();
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("MORTGAGE" + "\n" + "--------");
+        System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
 }
